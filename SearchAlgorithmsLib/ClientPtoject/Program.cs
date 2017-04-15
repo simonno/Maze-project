15ad6ced88816216d8048cd23438c2821e4afb4b
SYSTEM <SYSTEM@NOAM>
2017-04-15 15:59:17 +0300

    check

--- SearchAlgorithmsLib/ClientPtoject/Program.cs
+++ SearchAlgorithmsLib/ClientPtoject/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading.Tasks;
-using ControllerLib;
 
 namespace ClientPtoject
 {
@@ -38,9 +32,6 @@ namespace ClientPtoject
                 }
             }
             client.Close();
-
-
-
         }
     }
 }
