d54888633b36bf9065f4ddb31c91d3fedfc866be
meital.birka <meital.birka@gmail.com>
2017-04-15 15:57:54 +0300

    try to upload

--- SearchAlgorithmsLib/ControllerLib/StartCommand.cs
+++ SearchAlgorithmsLib/ControllerLib/StartCommand.cs
@@ -17,6 +17,7 @@ namespace ControllerLib
         public override string Execute(string[] args, TcpClient client = null)
         {
             string name = args[0];
+
             int rows = int.Parse(args[1]);
             int cols = int.Parse(args[2]);
             model.Start(name, rows, cols);
