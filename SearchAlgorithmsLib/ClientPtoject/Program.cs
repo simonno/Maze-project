13babb57ca761b48904637b434d5bdda54e5c616
meital.birka <meital.birka@gmail.com>
2017-04-15 11:10:40 +0300

    client

---
+++ SearchAlgorithmsLib/ClientPtoject/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using ControllerLib;
+
+namespace ClientPtoject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            TcpClient client = new TcpClient();
+            client.Connect(ep);
+            Console.WriteLine("You are connected");
+            string commandLine="" ;
+            string answerServer;
+
+            while (!commandLine.Equals("close"))
+            {
+                Console.Write("Please enter a command: ");
+
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader readerFromServer = new StreamReader(stream))
+                using (StreamWriter writerToServer = new StreamWriter(stream))
+                {
+                    commandLine = Console.ReadLine();
+                    writerToServer.Write(commandLine);
+                     answerServer = readerFromServer.ReadLine();
+                    Console.WriteLine("Result = " + answerServer);
+
+                }
+            }
+            client.Close();
+
+
+
+        }
+    }
+}
+
