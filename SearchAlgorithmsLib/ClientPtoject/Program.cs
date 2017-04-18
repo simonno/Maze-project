ea903709b9b5ce45e64b66ed68bef75a68f2da7f
meital.birka <meital.birka@gmail.com>
2017-04-18 21:56:01 +0300

    client trying

--- SearchAlgorithmsLib/ClientPtoject/Program.cs
+++ SearchAlgorithmsLib/ClientPtoject/Program.cs
@@ -22,68 +22,76 @@ namespace ClientPtoject
             string answerServer;
             try
             {
-               // while (!commandLine.Equals("close"))
+                // while (!commandLine.Equals("close"))
                 //{
-                    Console.Write("Please enter a command: ");
+                Console.Write("Please enter a command: ");
 
-                    using (NetworkStream stream = client.GetStream())
-                    using (StreamReader reader = new StreamReader(stream))
-                    using (StreamWriter writer = new StreamWriter(stream))
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader reader = new StreamReader(stream))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    commandLine = Console.ReadLine();
+
+                    writer.AutoFlush = true;
+                    writer.WriteLine(commandLine);
+                    // if (commandLine.Length>4&&(
+                    // commandLine.Substring(0, 5).Equals("solve")
+                    //|| commandLine.Substring(0, 8).Equals("generate") ))
+                    // {//single player
+                    //     answerServer = reader.ReadLine();
+                    //     answerServer = answerServer.Replace("@", System.Environment.NewLine);
+                    //     //string a = (string)JsonConvert.DeserializeObject(answerServer);
+                    //     Console.WriteLine("Result = " + answerServer);
+                    //     client.Close();
+                    // }
+                    // else
+                    // {//multi player
+                    //     Task taskRead = new Task(() =>
+                    //     {
+                    //         while (true)
+                    //         {
+                    //             try
+                    //             {
+                    //                 answerServer = reader.ReadLine();
+                    //                 answerServer = answerServer.Replace("@", System.Environment.NewLine);
+                    //                 Console.WriteLine("Result = " + answerServer);
+                    //             }
+                    //             catch (SocketException)
+                    //             {
+                    //                 break;
+                    //             }
+                    //         }
+                    //         Console.WriteLine("stop reading");
+                    //     });
+                    //     taskRead.Start();
+
+                    //     Task taskWrite = new Task(() =>
+                    //     {
+                    //         while (true)
+                    //         {
+                    //             try
+                    //             {
+                    //                 writer.AutoFlush = true;
+                    //                 writer.WriteLine(commandLine);
+                    //             }
+                    //             catch (SocketException)
+                    //             {
+                    //                 break;
+                    //             }
+                    //         }
+                    //         Console.WriteLine("stop writing");
+
+                    // });
+                    // taskWrite.Start();
+
+                    while (true)
                     {
-                        commandLine = Console.ReadLine();
+                        answerServer = reader.ReadLine();
 
-                        writer.AutoFlush = true;
-                        writer.WriteLine(commandLine);
-                        if (commandLine.Substring(0, 8).Equals("generate")
-                             || commandLine.Substring(0, 5).Equals("solve"))
-                        {//single player
-                            answerServer = reader.ReadLine();
-                            answerServer = answerServer.Replace("@", System.Environment.NewLine);
-                            //string a = (string)JsonConvert.DeserializeObject(answerServer);
-                            Console.WriteLine("Result = " + answerServer);
-                            client.Close();
-                        }
-                        else
-                        {//multi player
-                            Task taskRead = new Task(() =>
-                            {
-                                while (true)
-                                {
-                                    try
-                                    {
-                                        answerServer = reader.ReadLine();
-                                        answerServer = answerServer.Replace("@", System.Environment.NewLine);
-                                        Console.WriteLine("Result = " + answerServer);
-                                    }
-                                    catch (SocketException)
-                                    {
-                                        break;
-                                    }
-                                }
-                                Console.WriteLine("stop reading");
-                            });
-                            taskRead.Start();
-
-                            Task taskWrite = new Task(() =>
-                            {
-                                while (true)
-                                {
-                                    try
-                                    {
-                                        writer.AutoFlush = true;
-                                        writer.WriteLine(commandLine);
-                                    }
-                                    catch (SocketException)
-                                    {
-                                        break;
-                                    }
-                                }
-                                Console.WriteLine("stop writing");
-                            });
-                            taskWrite.Start();
-                        }
                     }
-               // }
+                }
+
+                // }
             }
             catch (SocketException ex)
             {
