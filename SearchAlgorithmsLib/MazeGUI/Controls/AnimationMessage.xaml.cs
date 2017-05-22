bf5b20d5c2bbc98eb6f675a94d5f94227a6eb532
meital birka <meital.birka@gmail.com>
2017-05-23 00:37:19 +0300

    waiting

---
+++ SearchAlgorithmsLib/MazeGUI/Controls/AnimationMessage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeGUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for AnimationMessage.xaml
+    /// </summary>
+    public partial class AnimationMessage : UserControl
+    {
+        public AnimationMessage()
+        {
+            InitializeComponent();
+        }
+
+        public string MassageText
+        {
+            get { return (string)GetValue(MassageTextProperty); }
+            set { SetValue(MassageTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for QuestionText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MassageTextProperty =
+            DependencyProperty.Register("MassageText", typeof(string),
+                typeof(AnimationMessage), new PropertyMetadata("?", OnMassageTextChanged));
+
+        private static void OnMassageTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is AnimationMessage animationMessage)
+            {
+                animationMessage.OnMassageTextChanged();
+            }
+        }
+
+        private void OnMassageTextChanged()
+        {
+            text.Text = MassageText;
+        }
+        //public string LeftBtnContent
+        //{
+        //    get { return (string)GetValue(LeftBtnContentProperty); }
+        //    set { SetValue(LeftBtnContentProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for LeftBtnContent.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty LeftBtnContentProperty =
+        //    DependencyProperty.Register("LeftBtnContent", typeof(string), typeof(AnimationMessage), new PropertyMetadata("?", OnLeftBtnContentChanged));
+
+        //private static void OnLeftBtnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    if (d is AnimationMessage animationMessage)
+        //    {
+        //        animationMessage.OnLeftBtnContentChanged();
+        //    }
+        //}
+
+        //private void OnLeftBtnContentChanged()
+        //{
+        //    btnLeft.Content = LeftBtnContent;
+        //}
+    }
+}
---
+++ SearchAlgorithmsLib/MazeGUI/MultiGame/WaitAnimation.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MazeGUI.MultiGame
+{
+    /// <summary>
+    /// Interaction logic for WaitAnimation.xaml
+    /// </summary>
+    public partial class WaitAnimation : Window
+    {
+        public WaitAnimation()
+        {
+            InitializeComponent();
+        }
+        public bool Choose { get; set; }
+        public string MessageText { get; private set; }
+        public string LeftBtnContent { get; private set; }
+
+        public WaitAnimation(string message, string leftBtnContent)
+        {
+            text.Text = "Loading";
+           // MessageText = message;
+            LeftBtnContent = leftBtnContent;
+            InitializeComponent();
+            DataContext = this;
+        }
+        private void BtnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            Choose = false;
+            Close();
+        }
+        
+    }
+}
