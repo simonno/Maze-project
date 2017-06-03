43a3b8aed0e659aca2e41f0aecf15525b61c1922
meital.birka <meital.birka@gmail.com>
2017-06-03 17:00:39 +0300

    register Page

---
+++ SearchAlgorithmsLib/WebApplication1/toolBar.js
@@ -0,0 +1,11 @@
+﻿document.getElementById("toolMenu").innerHTML =
+    '<ul>' +
+' <a href="SingleGamePage.html">Single Game</a>' +
+' <a href="SettingsPage.html">Settings</a>' +
+' <a href="UserRanking.html">Rankings</a>' +
+' <a href="RegisterPage.html">Register</a>' +
+' <a href="MultiGamePage.html">MultiPlayer</a>' +
+   
+    '</ul>';
+
+
