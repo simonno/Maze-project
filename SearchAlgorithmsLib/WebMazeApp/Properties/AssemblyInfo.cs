fe82ec1da65d788ab386ad881ca48b43e0927c50
SYSTEM <SYSTEM@NOAM>
2017-06-06 18:32:28 +0300

    edit booststrap

--- SearchAlgorithmsLib/WebMazeApp/Properties/AssemblyInfo.cs
+++ SearchAlgorithmsLib/WebMazeApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("WebMazeApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("WebMazeApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("411f1cef-f2f9-40b3-a02d-96aad342d4e4")]
+[assembly: Guid("4adc1bb6-5b96-43ab-b28b-4cbe078ca821")]
 
 // Version information for an assembly consists of the following four values:
 //
