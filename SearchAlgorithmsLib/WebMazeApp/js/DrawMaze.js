d4468c5fce837f6cfff0bdd30fd33c162371a5aa
SYSTEM <SYSTEM@NOAM>
2017-06-08 15:31:08 +0300

    nav  bar

---
+++ SearchAlgorithmsLib/WebMazeApp/js/DrawMaze.js
@@ -0,0 +1,23 @@
+﻿function drawMaze(maze) {
+    var myCanvas = this;
+    var context = myCanvas[0].getcontext("2d");
+    var rows = maze.length;
+    var cols = maze[0].length;
+    var cellWidth = this.width / cols;
+    var cellHeight = this.height / rows;
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (maze[i][j] == 1) {
+                context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+            }
+        }
+    }
+    return this;
+}
+
+
+(function ($) {
+
+    $.fn.drawMaze = drawMaze;
+
+}(jQuery));
