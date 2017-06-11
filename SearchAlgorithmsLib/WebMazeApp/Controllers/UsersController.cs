1af257768e763e6fc380b3a4c46f80628a060bea
meital birka <meital.birka@live.biu.ac.il>
2017-06-11 23:58:00 +0300

    controller

---
+++ SearchAlgorithmsLib/WebMazeApp/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http;
+using WebMazeApp.Model;
+
+namespace WebMazeApp.Controllers
+{
+    public class UsersController : ApiController
+    {
+
+        private IUserManager userManager = new UserManager();
+
+        // GET: api/Products
+        public IEnumerable<User> GetAllUsers()
+        {
+            return userManager.GetAllUsers();
+        }
+
+        // GET: api/Products/5
+        public User GetUserById(int id)
+        {
+            return userManager.GetUserById(id);
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public User AddProduct(User p)
+        {
+            userManager.AddUser(p);
+            return p;
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+            userManager.DeleteUser(id);
+        }
+    }
+}
