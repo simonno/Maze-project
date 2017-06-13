2b73edbbcd0a8ec9f59175155c6347415e4a5724
DELL <DELL@NOAM>
2017-06-13 16:17:46 +0300

    push

--- SearchAlgorithmsLib/WebMazeApp/js/DrawMaze.js
+++ SearchAlgorithmsLib/WebMazeApp/js/DrawMaze.js
@@ -1,23 +1,33 @@
-﻿function drawMaze(maze) {
-    var myCanvas = this;
-    var context = myCanvas[0].getcontext("2d");
-    var rows = maze.length;
-    var cols = maze[0].length;
-    var cellWidth = this.width / cols;
-    var cellHeight = this.height / rows;
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            if (maze[i][j] == 1) {
-                context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
-            }
-        }
-    }
-    return this;
-}
-
-
+﻿
 (function ($) {
 
-    $.fn.drawMaze = drawMaze;
+    $.fn.drawMaze = function (maze) {
+        var canvas = this.get(0);
+        var context = canvas.getContext("2d");
+        var rows = maze.length;
+        var cols = maze[0].length;
+        var cellWidth = canvas.width / cols;
+        var cellHeight = canvas.height / rows;
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                if (maze[i][j] == 1) {
+                    context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                }
+            }
+        }
+        return this;
+    };
 
 }(jQuery));
+
+
+$(document).ready(function () {
+    maze = [[0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1],
+    [0, 1, 0, 1, 0, 1, 1],
+    [0, 1, 0, 1, 0, 0, 1],
+    [0, 1, 0, 1, 1, 0, 1],
+    [0, 1, 0, 1, 1, 0, 1],
+    [0, 0, 0, 0, 1, 1, 1]];
+    $("#mazeCanvas").drawMaze(maze);
+});
