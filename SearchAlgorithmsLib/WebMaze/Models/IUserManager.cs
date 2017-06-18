2006ccd8a55ef355e31aac71d172a51fddb7c7eb
meital birka <meital.birka@live.biu.ac.il>
2017-06-18 18:49:57 +0300

    merge

---
+++ SearchAlgorithmsLib/WebMaze/Models/IUserManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebMaze.Models
+{
+    interface IUserManager
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+        void AddUser(User p);
+     //  void UpdateUser(User p);
+        void DeleteUser(int id);
+        int Register(User user);
+        int Login(LoginData login);
+
+
+    }
+}
