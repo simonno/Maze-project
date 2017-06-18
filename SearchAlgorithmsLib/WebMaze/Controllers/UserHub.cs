b75738e1a292295912e24bb0fca3b9a80b51430a
DELL <DELL@NOAM>
2017-06-19 01:27:59 +0300

    ppp

--- SearchAlgorithmsLib/WebMaze/Controllers/UserHub.cs
+++ SearchAlgorithmsLib/WebMaze/Controllers/UserHub.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
 
 namespace WebMaze.Controllers
 {
     public class UserHub : Hub
     {
-        public void Hello()
+        private static ConcurrentDictionary<string, string> connectedPlayers =
+            new ConcurrentDictionary<string, string>();
+
+        private static ConcurrentDictionary<string, string> opponnents =
+            new ConcurrentDictionary<string, string>();
+
+
+        public void Register(string userName)
         {
-            Clients.All.hello();
+            connectedPlayers[userName] = Context.ConnectionId;
         }
+
+        public void Move(string userName, int direction)
+        {
+            string opponnentUsername = opponnents[userName];
+            string recipientId = connectedPlayers[recipientPhoneNum];
+            if (recipientId == null)
+                return;
+            Clients.Client(recipientId).gotMessage(senderPhoneNum, text);
+        }
+
     }
 }
