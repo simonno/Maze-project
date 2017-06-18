a3292f24b9f4eb07b28ad59cf8a4206704a925b3
DELL <DELL@NOAM>
2017-06-18 18:46:28 +0300

    push

---
+++ SearchAlgorithmsLib/WebMaze/js/NewGameValidation.js
@@ -0,0 +1,48 @@
+﻿
+$(document).ready(function () {
+    $.validator.setDefaults({
+        highlight: function (element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        }
+    });
+
+    // validate signup form on keyup and submit
+    $("#registerForm").validate({
+        rules: {
+            mazeName: {
+                required: true,
+                minlength: 2
+            },
+            mazeRows: {
+                required: true,
+                number: true
+            },
+            mazeCols: {
+                required: true,
+                number: true
+            },
+        },
+        messages: {
+            mazeName: {
+                required: "Enter a name for the maze",
+                minlength: jQuery.validator.format("Enter at least {0} characters"),
+            },
+            mazeRows: {
+                required: "Enter a number of rows for the maze",
+                number: jQuery.validator.format("Enter only numbers"),
+
+            },
+            mazeCols: {
+                required: "Enter a number of rows for the maze",
+                number: jQuery.validator.format("Enter only numbers"),
+            },
+        },
+        // specifying a submitHandler prevents the default submit, good for the demo
+        submitHandler: function () {
+            alert("submitted!");
+        },
+    });
+});
