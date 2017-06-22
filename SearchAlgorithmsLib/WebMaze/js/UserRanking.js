1ba0e4fbbac345496eb1e93f7e2ae864461cf208
DELL <DELL@NOAM>
2017-06-22 21:31:19 +0300

    push

--- SearchAlgorithmsLib/WebMaze/js/UserRanking.js
+++ SearchAlgorithmsLib/WebMaze/js/UserRanking.js
@@ -3,12 +3,14 @@
     $.getJSON(usersUrl, function (responseData) {
         
 
-        responseData.forEach(function (user) {
-            $("#rankTableBody").append("<tr><td>" + user.Id+ "</td>" +
-                "<td>" + user.Username + "</td>" +
-                "<td>" + user.Wins + "</td>" +
-                "<td>" + user.Losses + "</td></tr>");
+        $("#rankTable").DataTable({
+            data: responseData,
+            columns: [
+                { 'data' : 'Username' },
+                { 'data' : 'Wins' },
+                { 'data' : 'Losses' }
+            ] 
         });
     });
-    $("#rankTable").DataTable();
+    
 })
