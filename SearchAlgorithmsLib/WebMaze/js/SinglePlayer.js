f600c08f26a9048ba059d1bd2f5f55f1a89d75be
DELL <DELL@NOAM>
2017-06-23 13:58:41 +0300

    push

--- SearchAlgorithmsLib/WebMaze/js/SinglePlayer.js
+++ SearchAlgorithmsLib/WebMaze/js/SinglePlayer.js
@@ -66,6 +66,28 @@ $(document).ready(function () {
                     exitImage.src = "Images/exit1.png";
 
                     $("#mazeCanvas").drawMaze(rowsMaze, colsMaze, maze, startRow, startCol, exitRow, exitCol, playerImage, exitImage);
+                    $("#solveGameForm").fadeIn("slow");
+
+                    $(document).keydown(function (eve) {
+                        var keycode = eve.which;
+                        switch (keycode) {
+
+                            case 38: // Up
+                                alert("Up");
+                                break;
+                            case 37: // Left
+                                alert("Left");
+                                break;
+                            case 39: // Right
+                                alert("Right");
+                                break;
+                            case 40: // Down
+                                alert("Down");
+                                break;
+                            default:
+                                break;
+                        }
+                    });
                 }
             });
         },
@@ -88,26 +110,4 @@ $("#btnSolveGame").click(function () {
             // TODO SOLVE 
         }
     });
-});
-
-$(document).keypress(function (event) {
-
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    switch (keycode) {
-        case '38': // Up
-            alert("Up");
-            break;
-        case '37': // Left
-            alert("Left");
-            break;
-        case '39': // Right
-            alert("Right");
-            break;
-        case '40': // Down
-            alert("Down");
-            break;
-        default:
-            break;
-    }
-
 });
